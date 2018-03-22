11f78452f990340fe1c4bdd5b5a8629b5d749b85
Nicklas Drejergaard <nicklas@slplarsen.dk>
2018-03-22 09:23:28 +0100

    wrote UDP_client and UDP_server

--- Exercise_7/UDP_server/UDP_server/UDP_client.py
+++ Exercise_7/UDP_server/UDP_server/UDP_client.py
@@ -9,11 +9,28 @@ def main(argv):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     sock.bind((HOST_IP, PORT))
-
-    sock.listen(5)
-
     print("Server socket opened")
 
-    clisock, addr = sock.accept()
+    while True:
+
+        message = input("If you wish to know the uptime, please write u or U. If you wich to know the average loadtime, please write l or L: ")
+
+        if message == "close":
+            print("closing socket")
+            sock.close()
+        else:
+            print("sending %s\n" % message)
+            sent = sock. sendto(message, HOST_IP)
+
+            print("Waiting to recieve\n")
+            data, server = sock.recvfrom(BUFSIZE)
+
+            if message == 'U' or message == 'u':
+                print("The uptime is: %s\n" % data)
+
+            elif message == 'L' or message == 'l':
+                print("The loadaverage is: %s\n" % data)
+
+    
 
 
--- Exercise_7/UDP_server/UDP_server/UDP_server.py
+++ Exercise_7/UDP_server/UDP_server/UDP_server.py
@@ -1,1 +1,39 @@
+import sys
+import socket
 
+HOST_IP = "10.0.0.1"
+PORT = 9000
+BUFSIZE = 2048
+UP = /proc/uptime
+LOAD = /proc/loadavg
+
+def main(argv):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    sock.bind((HOST_IP, PORT))
+
+    sock.listen(5)
+
+    print ("Server socket opened.\n")
+
+    while True:
+        print ("waiting to receive a message.\n")
+        data, address = sock.recvfrom(BUFSIZE)
+
+        print ("received %s bytes from %s" % (len(data), address))
+
+        print (data)
+
+        if data == 'U' or data == 'u':
+            file = open(UP, 'r')
+            filedata = file.read(BUFSIZE)
+
+            sent = sock.sendto(data, address)
+            print ("sent %s bytes back to %s" % (sent, address))
+
+        elif data == 'L' or data == 'l':
+            file = open(LOAD, 'r')
+            filedata = file.read(BUFSIZE)
+
+            sent = sock.sendto(data, address)
+            print ("sent %s bytes back to %s" % (sent, address))
